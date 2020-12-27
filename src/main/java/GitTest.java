fc3add901a33288c3741b0bbadf4f6a9fd93afbd
anlexander <610407517@qq.com>
2020-12-27 21:13:34 +0800

    首次提交到本地库

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author anlexander
+ * @date 2020/12/27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("this is my git test!!!!");
+    }
+}
